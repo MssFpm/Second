75c590168331a2d89b3f2a363db33f6062022cc5
Pavel Astraukh <paladin111333@yandex.ru>
2012-11-17 13:40:00 +0300

    Initial Commit

---
+++ Second/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Second
+//
+//  Created by Pavel Astraukh on 17.11.12.
+//  Copyright (c) 2012 Pavel Astraukh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
